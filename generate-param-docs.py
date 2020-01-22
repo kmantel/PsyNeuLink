11080c4204445b1e3dbda38b49ead60c27da8c83
Katherine Mantel <katherine@mantel.pw>
2020-01-21 22:12:47 -0500

    updates - split it?

--- generate-param-docs.py
+++ generate-param-docs.py
@@ -3,12 +3,30 @@ import enum
 import inspect
 import logging
 import numpy as np
+import os
 import psyneulink as pnl
 import re
 import subprocess
 import types
+import typing
 
 from psyneulink.core.globals.utilities import is_instance_or_subclass
+
+
+def indent_str(string, num=0, indent_char=' ', indent_char_multiplier=4):
+    return '{0}{1}'.format(
+        '{0}'.format(
+            ''.join([indent_char for i in range(indent_char_multiplier)])
+        ).join(
+            ['' for i in range(num + 1)]
+        ),
+        string
+    )
+
+
+def generate_param_desc_string(*args, indent=3):
+    return '\n'.join([indent_str(x, indent) for x in args])
+
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
@@ -18,33 +36,52 @@ ch.setFormatter(logging.Formatter('%(message)s'))
 logger.addHandler(ch)
 
 type_to_type_str = {
-    int: 'int',
-    float: 'float',
+    int: '``int``',
+    float: '``float``',
     None: '',
-    str: 'str',
-    list: 'list',
-    bool: 'bool',
+    str: '``str``',
+    list: '``list``',
+    bool: '``bool``',
+    types.FunctionType: '``types.FunctionType``'
+
 }
 
 manual_descriptions = {
     pnl.Component: 'The `Parameters` that are associated with all `Components`',
+    id(pnl.Component.parameters.is_finished_flag): (
+        generate_param_desc_string(
+            'internal parameter used by some Component types to track previous status of is_finished() method,',
+            'or to set the status reported by the is_finished (see `is_finished <Component_Is_Finished>`',
+        )
+    ),
+    id(pnl.ContrastiveHebbianMechanism.parameters.phase_convergence_threshold): (
+        generate_param_desc_string(
+            'internal parameter, used by is_converged;  assigned the value of the termination_threshold for',
+            'the current `phase of execution <ContrastiveHebbian_Execution>`.',
+        )
+    ),
+    id(pnl.InputPort.parameters.shadow_inputs): (
+        generate_param_desc_string(
+            'specifies whether InputPort shadows inputs of another InputPort;',
+            'if not None, must be assigned another InputPort',
+        )
+    ),
+    id(pnl.Port_Base.parameters.require_projection_in_composition): (
+        generate_param_desc_string(
+            'specifies whether the InputPort requires a projection when instantiated in a Composition;',
+            'if so, but none exists, a warning is issued.',
+        )
+    ),
+}
+
+manual_parameter_types = {
+    'random_state': '``numpy.random.RandomState``'
 }
 
 
 def replace_numpy_str(nparr):
     nparr = re.sub(r'\n *', ' ', repr(nparr))
     return 'numpy.{0}'.format(nparr)
-
-
-def indent_str(s, num=0, indent_char=' ', indent_char_multiplier=4):
-    return '{0}{1}'.format(
-        '{0}'.format(
-            ''.join([indent_char for i in range(indent_char_multiplier)])
-        ).join(
-            ['' for i in range(num + 1)]
-        ),
-        s
-    )
 
 
 def preprocess_params_list(params):
@@ -60,14 +97,61 @@ def preprocess_params_list(params):
     return fixed_params + sorted([x for x in params if x.name not in fixed_strings], key=lambda p: p.name)
 
 
+def parse_type_to_type_string(typ):
+    if is_instance_or_subclass(typ, pnl.Function):
+        typ = '`{0}`'.format(pnl.Function.__name__)
+
+    if is_instance_or_subclass(typ, (aenum.enum, enum.Enum)):
+        typ = '`{0}`'.format(typ.__name__)
+
+    if inspect.isclass(typ) and issubclass(typ, np.ndarray):
+        typ = '``{0}.{1}``'.format(typ.__module__, typ.__name__)
+
+    if isinstance(typ, list):
+        typ = [parse_type_to_type_string(x) for x in typ]
+
+    try:
+        typ = type_to_type_str[typ]
+    except KeyError:
+        pass
+
+    return typ
+
+
 def parse_default_value_and_type_from_param(param):
-    return parse_default_value_and_type(param.default_value)
+    default_val_string, typ = parse_default_value_and_type(param.default_value)
+
+    if isinstance(param._owner._owner, pnl.core.components.component.ComponentsMeta):
+        try:
+            # remove None out of optional
+            typ = [
+                x for x
+                in typing.get_type_hints(param._owner._owner.__init__)[param.name].__args__
+                # checking for NoneType not None
+                if x is not type(None)
+            ]
+            if len(typ) == 1:
+                typ = typ[0]
+        except KeyError:
+            pass
+        except AttributeError:
+            # tc.typecheck
+            pass
+
+    if typ is None:
+        try:
+            typ = manual_parameter_types[param.name]
+            pass
+        except KeyError:
+            pass
+
+    return default_val_string, parse_type_to_type_string(typ)
 
 
 def parse_default_value_and_type(default_value):
     if default_value is None:
         default_val_string = 'None'
-        typ = ''
+        typ = None
     elif isinstance(default_value, str):
         # camelcase to uppercase-underscore (std format of keywords)
         default_val_string = re.sub('([a-z0-9])([A-Z])', r'\1_\2', default_value).upper()
@@ -82,6 +166,18 @@ def parse_default_value_and_type(default_value):
     elif isinstance(default_value, pnl.core.components.functions.optimizationfunctions.SampleIterator):
         default_val_string = '`{0}`'.format(default_value.__class__.__name__)
         typ = default_value.__class__
+    # elif isinstance(default_value, types.MethodType):
+    #     default_val_string = default_val_string.__qualname__
+    elif isinstance(default_value, types.BuiltinFunctionType):
+        if default_value.__module__ == 'builtins':
+            # just give standard type, like float or int
+            default_val_string = f'{default_value.__name__}'
+        else:
+            # some builtin modules are internally "_module"
+            # but are imported with "module"
+            default_val_string = f"{default_value.__module__.lstrip('_')}.{default_value.__name__}"
+        default_val_string = f'``{default_val_string}``'
+        typ = types.FunctionType
     elif isinstance(default_value, types.FunctionType):
         if '<lambda>' == default_value.__name__:
             default_val_string = re.sub('.*Parameter\\((.*?):(.*?)[,\\)].*', r'\1:\2', inspect.getsource(default_value)).strip()
@@ -126,24 +222,11 @@ def parse_default_value_and_type(default_value):
             default_val_string = str(default_value)
             typ = type(default_value)
 
-    if is_instance_or_subclass(typ, pnl.Function):
-        typ = '`{0}`'.format(pnl.Function.__name__)
-
-    if is_instance_or_subclass(typ, (aenum.enum, enum.Enum)):
-        typ = '`{0}`'.format(typ.__name__)
-
-    if inspect.isclass(typ) and issubclass(typ, np.ndarray):
-        typ = '{0}.{1}'.format(typ.__module__, typ.__name__)
-
-    try:
-        typ = type_to_type_str[typ]
-    except KeyError:
-        pass
-
     return default_val_string, typ
 
 
 def make_docstring_for_class(class_, module_fname):
+    module_content = open(module_fname, 'r', encoding='utf-8').read()
     new_params = []
 
     for param in class_.parameters:
@@ -193,13 +276,22 @@ def make_docstring_for_class(class_, module_fname):
         )
         sphinx_ref_str = f'.. _{manual_ref_str}:'
 
-        if sphinx_ref_str in open(module_fname, 'r', encoding='utf-8').read():
+        if sphinx_ref_str in module_content:
             ref_str = manual_ref_str
         else:
             ref_str = basic_ref_str
-        # import code
-        # code.interact(local=locals())
-        result += indent_str('see `{0} <{1}>`\n'.format(param.name, ref_str), 3)
+
+        # attempt to find existent description
+        # desc_str_pat = re.compile(rf'""".*{param.name}\n +[^(see)]', re.MULTILINE)
+        # desc_str_pat = re.compile(rf'(.|\n)*{param.name}', re.MULTILINE)
+        # match = desc_str_pat.findall(module_content)
+        try:
+            desc_str = manual_descriptions[id(param)]
+        except KeyError:
+            desc_str = indent_str('see `{0} <{1}>`'.format(param.name, ref_str), 3)
+
+        result += desc_str
+        result += '\n'
 
         default_val_string, typ = parse_default_value_and_type_from_param(param)
 
@@ -240,8 +332,9 @@ def print_all_pnl_docstrings():
 
                     logger.info('printing docstring for {0} residing in {1}'.format(item, module_fname))
 
-                    subprocess.run(['setup-param-docs.sh', module_fname])
-                    subprocess.run(['perl', 'substitute-param-docs.pl', module_fname, item.__name__, docstring])
+                    dir_name = os.path.dirname(os.path.abspath(__file__))
+                    subprocess.run([f'{dir_name}/setup-param-docs.sh', module_fname])
+                    subprocess.run(['perl', f'{dir_name}/substitute-param-docs.pl', module_fname, item.__name__, docstring])
 
                     logger.info(docstring)
                     logger.info('================')
