f86ecbe5297ed435fdab41196362d96d7bf3490e
Katherine Mantel <kmantel@princeton.edu>
2021-09-01 00:41:01 -0400

    JSON: make projections/edges as dummy nodes with functions

--- psyneulink/core/components/mechanisms/mechanism.py
+++ psyneulink/core/components/mechanisms/mechanism.py
@@ -1094,6 +1094,7 @@ from psyneulink.core.components.ports.port import \
     REMOVE_PORTS, PORT_SPEC, _parse_port_spec, PORT_SPECIFIC_PARAMS, PROJECTION_SPECIFIC_PARAMS
 from psyneulink.core.components.shellclasses import Mechanism, Projection, Port
 from psyneulink.core.globals.context import Context, ContextFlags, handle_external_context
+from psyneulink.core.globals.json import _get_variable_parameter_name
 # TODO: remove unused keywords
 from psyneulink.core.globals.keywords import \
     ADDITIVE_PARAM, EXECUTION_PHASE, EXPONENT, FUNCTION_PARAMS, \
@@ -4019,7 +4020,7 @@ class Mechanism_Base(Mechanism):
 
         function_model = self.function.as_mdf_model()
         # primary input port
-        function_model.args[MODEL_SPEC_ID_MDF_VARIABLE] = model.input_ports[0].id
+        function_model.args[_get_variable_parameter_name(self.function)] = model.input_ports[0].id
         model.functions.append(function_model)
 
         return model
