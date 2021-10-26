b50e2023fa8d8452f4db914e52ad9852583e23ab
Katherine Mantel <kmantel@princeton.edu>
2021-10-25 20:24:15 -0400

    JSON: don't crash PNL if modeci_mdf not available
    modeci_mdf is not supported on 32-bit python because of onnxruntime, but
    some psyneulink functionality is tested and should still be able to run

--- psyneulink/core/components/ports/inputport.py
+++ psyneulink/core/components/ports/inputport.py
@@ -525,7 +525,6 @@ import itertools
 import numbers
 import warnings
 
-import modeci_mdf.mdf as mdf
 import numpy as np
 import typecheck as tc
 
@@ -1423,6 +1422,8 @@ class InputPort(Port_Base):
         return Port_Base._get_port_function_value(owner=owner, function=function, variable=variable)
 
     def as_mdf_model(self):
+        import modeci_mdf.mdf as mdf
+
         return mdf.InputPort(
             id=parse_valid_identifier(self.name),
             shape=str(self.defaults.variable.shape),
