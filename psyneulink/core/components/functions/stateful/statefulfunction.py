2ef5d95782234d81f60ba68671e46c83f09531c2
Katherine Mantel <kmantel@princeton.edu>
2022-10-14 01:13:53 -0400

    StatefulFunction: change single-value 2d initializer reshapes to 1d

--- psyneulink/core/components/functions/stateful/statefulfunction.py
+++ psyneulink/core/components/functions/stateful/statefulfunction.py
@@ -46,6 +46,9 @@ def _match_stateful_attribute_shape(attr_value, variable):
     except ValueError:
         reshaped = attr_value.reshape(variable.shape).copy()
 
+    if np.asarray(attr_value).shape == (1,) and reshaped.shape == (1, 1):
+        reshaped = reshaped[0]
+
     return reshaped
 
 
