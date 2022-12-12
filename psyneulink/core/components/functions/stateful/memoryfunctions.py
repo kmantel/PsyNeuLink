91fbaf71775299d31382918fbc21934b03f05321
Katherine Mantel <kmantel@princeton.edu>
2022-12-12 18:34:36 -0500

    Buffer: reset: set previous_value

--- psyneulink/core/components/functions/stateful/memoryfunctions.py
+++ psyneulink/core/components/functions/stateful/memoryfunctions.py
@@ -303,6 +303,7 @@ class Buffer(MemoryFunction):  # -----------------------------------------------
 
         else:
             value = self._initialize_previous_value(previous_value, context=context)
+            self.parameters.previous_value._set(value, context)
 
         self.parameters.value.set(value, context, override=True)
         return value
