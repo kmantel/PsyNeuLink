532914ea2aecd0b9eeed8f062ef90481b2323968
Katherine Mantel <kmantel@princeton.edu>
2024-06-11 03:53:29 +0000

    Mechanism: correct indication that input_ports arg was specified by user
    If input ports specifying a shape occur before others that do not, no
    shape validation occurred.

--- psyneulink/core/components/mechanisms/mechanism.py
+++ psyneulink/core/components/mechanisms/mechanism.py
@@ -1913,7 +1913,10 @@ class Mechanism_Base(Mechanism):
             if isinstance(parsed_input_port_spec, dict):
                 try:
                     mech_variable_item = parsed_input_port_spec[VALUE]
-                    if parsed_input_port_spec[VARIABLE] is None:
+                    if (
+                        parsed_input_port_spec[VARIABLE] is None
+                        and not input_port_variable_was_specified  # don't unset True from another port
+                    ):
                         input_port_variable_was_specified = False
                 except KeyError:
                     pass
