kmantel/PsyNeuLink

commit 19481241a85a2d69b11e1ada96afa388e3274679

condition: fix pnl-specific names in condition.py stub and __all__ (#3089)

psyneulink/core/scheduling/condition.py
@@ -25,28 +25,21 @@ from psyneulink.core.globals.utilities import parse_valid_identifier, toposort_k
 
 
 __all__ = [  # noqa: F822  (dynamically generated)
-    'AbsoluteCondition', 'AddEdgeTo', 'AfterCall',
-    'AfterConsiderationSetExecution', 'AfterEnvironmentSequence',
-    'AfterEnvironmentStateUpdate', 'AfterNCalls', 'AfterNCallsCombined',
-    'AfterNConsiderationSetExecutions', 'AfterNEnvironmentSequences',
-    'AfterNEnvironmentStateUpdates', 'AfterNPasses', 'AfterNRuns',
-    'AfterNTimeSteps', 'AfterNTrials', 'AfterNode', 'AfterNodes',
-    'AfterPass', 'AfterRun', 'AfterTimeStep', 'AfterTrial', 'All',
-    'AllHaveRun', 'Always', 'And', 'Any', 'AtConsiderationSetExecution',
-    'AtEnvironmentSequence', 'AtEnvironmentSequenceNStart',
-    'AtEnvironmentSequenceStart', 'AtEnvironmentStateUpdate',
-    'AtEnvironmentStateUpdateNStart', 'AtEnvironmentStateUpdateStart',
-    'AtNCalls', 'AtPass', 'AtRun', 'AtRunNStart', 'AtRunStart',
-    'AtTimeStep', 'AtTrial', 'AtTrialNStart', 'AtTrialStart',
-    'BeforeConsiderationSetExecution', 'BeforeEnvironmentStateUpdate',
-    'BeforeNCalls', 'BeforeNode', 'BeforeNodes', 'BeforePass',
-    'BeforeTimeStep', 'BeforeTrial', 'CompositeCondition', 'Condition',
-    'ConditionBase', 'ConditionError', 'ConditionSet',
-    'CustomGraphStructureCondition', 'EveryNCalls', 'EveryNPasses',
-    'GraphStructureCondition', 'JustRan', 'NWhen', 'Never', 'Not',
-    'Operation', 'Or', 'RemoveEdgeFrom', 'Threshold', 'TimeInterval',
-    'TimeTermination', 'WhenFinished', 'WhenFinishedAll',
-    'WhenFinishedAny', 'When', 'While', 'WhileNot', 'WithNode',
+    'AbsoluteCondition', 'AddEdgeTo', 'AfterCall', 'AfterNCalls',
+    'AfterNCallsCombined', 'AfterNode', 'AfterNodes', 'AfterNPasses',
+    'AfterNRuns', 'AfterNTimeSteps', 'AfterNTrials', 'AfterPass',
+    'AfterRun', 'AfterTimeStep', 'AfterTrial', 'All', 'AllHaveRun',
+    'Always', 'And', 'Any', 'AtNCalls', 'AtPass', 'AtRun',
+    'AtRunNStart', 'AtRunStart', 'AtTimeStep', 'AtTrial',
+    'AtTrialNStart', 'AtTrialStart', 'BeforeNCalls', 'BeforeNode',
+    'BeforeNodes', 'BeforePass', 'BeforeTimeStep', 'BeforeTrial',
+    'CompositeCondition', 'Condition', 'ConditionBase',
+    'ConditionError', 'ConditionSet', 'CustomGraphStructureCondition',
+    'EveryNCalls', 'EveryNPasses', 'GraphStructureCondition', 'JustRan',
+    'Never', 'Not', 'NWhen', 'Operation', 'Or', 'RemoveEdgeFrom',
+    'Threshold', 'TimeInterval', 'TimeTermination', 'When',
+    'WhenFinished', 'WhenFinishedAll', 'WhenFinishedAny', 'While',
+    'WhileNot', 'WithNode',
 ]
 
 

tests/scheduling/test_condition.py
@@ -25,7 +25,15 @@ logger = logging.getLogger(__name__)
 
 class TestModule:
     def test_all_attr_parity(self):
-        missing = set(gs.condition.__all__) - set(pnl.core.scheduling.condition.__all__)
+        missing = {
+            c for c
+            in set(gs.condition.__all__) - set(pnl.core.scheduling.condition.__all__)
+            if (
+                'ConsiderationSetExecution' not in c
+                and 'EnvironmentStateUpdate' not in c
+                and 'EnvironmentSequence' not in c
+            )
+        }
         assert len(missing) == 0, (f'Conditions in graph_scheduler must be added to psyneulink condition.py: {missing}')
 
 

commit 6758145918a39611d0f0ee7f8345031b6765f7c2

condition: restore compatibility with graph-scheduler<1.2
broken by #3089 which hardcoded graph structure condition classes into
__all__, causing psyneulink to crash on import

psyneulink/core/scheduling/condition.py
@@ -25,21 +25,21 @@ from psyneulink.core.globals.utilities import parse_valid_identifier, toposort_k
 
 
 __all__ = [  # noqa: F822  (dynamically generated)
-    'AbsoluteCondition', 'AddEdgeTo', 'AfterCall', 'AfterNCalls',
-    'AfterNCallsCombined', 'AfterNode', 'AfterNodes', 'AfterNPasses',
-    'AfterNRuns', 'AfterNTimeSteps', 'AfterNTrials', 'AfterPass',
-    'AfterRun', 'AfterTimeStep', 'AfterTrial', 'All', 'AllHaveRun',
-    'Always', 'And', 'Any', 'AtNCalls', 'AtPass', 'AtRun',
-    'AtRunNStart', 'AtRunStart', 'AtTimeStep', 'AtTrial',
-    'AtTrialNStart', 'AtTrialStart', 'BeforeNCalls', 'BeforeNode',
-    'BeforeNodes', 'BeforePass', 'BeforeTimeStep', 'BeforeTrial',
-    'CompositeCondition', 'Condition', 'ConditionBase',
-    'ConditionError', 'ConditionSet', 'CustomGraphStructureCondition',
-    'EveryNCalls', 'EveryNPasses', 'GraphStructureCondition', 'JustRan',
-    'Never', 'Not', 'NWhen', 'Operation', 'Or', 'RemoveEdgeFrom',
-    'Threshold', 'TimeInterval', 'TimeTermination', 'When',
-    'WhenFinished', 'WhenFinishedAll', 'WhenFinishedAny', 'While',
-    'WhileNot', 'WithNode',
+    'AbsoluteCondition', 'AfterCall', 'AfterNCalls', 'AfterNCallsCombined',
+    'AfterNPasses', 'AfterNRuns', 'AfterNTimeSteps', 'AfterNTrials',
+    'AfterPass', 'AfterRun', 'AfterTimeStep', 'AfterTrial', 'All', 'AllHaveRun',
+    'Always', 'And', 'Any', 'AtNCalls', 'AtPass', 'AtRun', 'AtRunNStart',
+    'AtRunStart', 'AtTimeStep', 'AtTrial', 'AtTrialNStart', 'AtTrialStart',
+    'BeforeNCalls', 'BeforePass', 'BeforeTimeStep', 'BeforeTrial',
+    'CompositeCondition', 'Condition', 'ConditionError', 'ConditionSet',
+    'EveryNCalls', 'EveryNPasses', 'JustRan', 'NWhen', 'Never', 'Not', 'Or',
+    'Threshold', 'TimeInterval', 'TimeTermination', 'When', 'WhenFinished',
+    'WhenFinishedAll', 'WhenFinishedAny', 'While', 'WhileNot',
+]
+_gsc__all__ = [
+    'AddEdgeTo', 'AfterNode', 'AfterNodes', 'BeforeNode', 'BeforeNodes',
+    'ConditionBase', 'CustomGraphStructureCondition', 'GraphStructureCondition',
+    'Operation', 'RemoveEdgeFrom', 'WithNode',
 ]
 
 
@@ -60,6 +60,7 @@ else:
                 f'MDF support not yet implemented for {type(self)}'
             )
     condition_class_parents.append(ConditionBase)
+    __all__.extend(_gsc__all__)
 
 
 try: