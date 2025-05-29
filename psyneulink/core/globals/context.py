62fb5b098a2967fc2bca1827f24dcf148ecedc06
Katherine Mantel <kmantel@princeton.edu>
2025-05-29 06:02:27 +0000

    Context: check type equivalence not isinstance

--- psyneulink/core/globals/context.py
+++ psyneulink/core/globals/context.py
@@ -705,7 +705,7 @@ def handle_external_context(
         def wrapper(*args, context=None, **kwargs):
             eid = execution_id
 
-            if context is not None and not isinstance(context, Context):
+            if context is not None and not type(context) is Context:
                 try:
                     eid = context.default_execution_id
                 except AttributeError:
@@ -714,7 +714,7 @@ def handle_external_context(
             else:
                 try:
                     if args[context_arg_index] is not None:
-                        if isinstance(args[context_arg_index], Context):
+                        if type(args[context_arg_index]) is Context:
                             context = args[context_arg_index]
                         else:
                             try:
