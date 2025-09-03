daa8df15901a08b5cb42c3f5e280d71a7d9dfdfd
Katherine Mantel <kmantel@princeton.edu>
2025-09-03 05:13:55 +0000

    gru: patch imports for no-torch installed

--- psyneulink/library/compositions/grucomposition/__init__.py
+++ psyneulink/library/compositions/grucomposition/__init__.py
@@ -1,15 +1,10 @@
-# from .grucomposition import *
-# from .pytorchGRUwrappers import *
-# __all__ = list(grucomposition.__all__)
-# __all__.extend(pytorchGRUwrappers.__all__)
+from . import grucomposition
+from .grucomposition import *
+__all__ = list(grucomposition.__all__)
 
-# CONSIDER SETTING torch_available HERE AND IMPORTING THAT INTO grucomposition
 try:
     import torch
-    from . import grucomposition
-    from .grucomposition import *
     from .pytorchGRUwrappers import *
-    __all__ = list(grucomposition.__all__)
     __all__.extend(pytorchGRUwrappers.__all__)
 except:
     pass
