2a5471302da7803ae1f51ba1e701d7b60702d145
Katherine Mantel <kmantel@princeton.edu>
2026-05-27 10:44:22 +0000

    fixup! WIP treewide: support >2D input for Components (non-LLVM)

--- tests/composition/test_learning.py
+++ tests/composition/test_learning.py
@@ -777,37 +777,6 @@ class TestStructural:
                              })
             np.testing.assert_allclose(comp.results,[[[1.], [1.]], [[2.42], [3.38]]])
 
-        def test_dict_target_spec_divering_pathways_with_only_one_target(self):
-            # First test with both targets (but use default_variale for second for comparison with missing target)
-            A = TransferMechanism(name="diverging-learning-pathways-mech-A")
-            B = TransferMechanism(name="diverging-learning-pathways-mech-B")
-            C = TransferMechanism(name="diverging-learning-pathways-mech-C")
-            D = TransferMechanism(name="diverging-learning-pathways-mech-D")
-            E = TransferMechanism(name="diverging-learning-pathways-mech-E")
-            comp1 = Composition()
-            p1 = comp1.add_backpropagation_learning_pathway(pathway=[A,B,C])
-            p2 = comp1.add_backpropagation_learning_pathway(pathway=[A,D,E])
-            comp1.learn(inputs={A: 1.0,
-                                p1.target: 0.0,
-                                p2.target: 2.0
-                                },
-                        num_trials=2)
-            np.testing.assert_allclose(comp1.results,[[[1.], [1.]], [[0.81], [1.21]]])
-
-            F = TransferMechanism(name="diverging-learning-pathways-mech-F")
-            G = TransferMechanism(name="diverging-learning-pathways-mech-G")
-            H = TransferMechanism(name="diverging-learning-pathways-mech-H")
-            I = TransferMechanism(name="diverging-learning-pathways-mech-I")
-            J = TransferMechanism(name="diverging-learning-pathways-mech-J")
-            comp2 = Composition()
-            p3 = comp2.add_backpropagation_learning_pathway(pathway=[F,G,H])
-            p4 = comp2.add_backpropagation_learning_pathway(pathway=[F,I,J])
-            # Call learn with missing spec for p3.target;  should use default_variable
-            comp2.learn(inputs={F: 1.0,
-                                p4.target: 2.0
-                                },
-                        num_trials=2)
-            np.testing.assert_allclose(comp2.results, comp1.results)
 
         # The input sizes were picked because the lengths conflict in set:
         # >>> print({10, 2}, {2, 10})
