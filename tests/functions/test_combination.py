18d18a522223e68eccb709e33b7ca41c0f47fca0
Katherine Mantel <kmantel@princeton.edu>
2026-08-07 12:28:01 +0000

    TMP: try paper over LLVMBuilderContext problem

--- tests/functions/test_combination.py
+++ tests/functions/test_combination.py
@@ -203,6 +203,10 @@ RANDh_A = {
 }
 
 
+# set by _rand. expecting to be here during collection and not during test runs
+pnl.LLVMBuilderContext.clear_global()
+
+
 @pytest.mark.benchmark(group="ReduceFunction")
 @pytest.mark.function
 @pytest.mark.combination_function
