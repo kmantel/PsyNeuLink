3ac4f302685843d32f0078f3d4cca05f4675731c
Katherine Mantel <kmantel@princeton.edu>
2026-09-18 10:38:02 +0000

    wip

--- tests/ports/test_parameter_ports.py
+++ tests/ports/test_parameter_ports.py
@@ -93,13 +93,14 @@ class TestParameterPorts:
                '(assignment attempted for TransferMechanism-0[slope]).' in str(error.value)
 
     @pytest.mark.parametrize(
-        'mech_type, mech_kwargs, param_name', [
-            *[(pnl.ExternalMemoryMechanism, {'field_type': pnl.FieldType.KEY, 'field_shape': (1, 1), 'field_memory': [[[0], [0]]]}, p) for p in ['matrix', 'decay_rate']],
+        'mech_type, mech_kwargs, param', [
+            *[(pnl.ExternalMemoryMechanism, {'field_type': pnl.FieldType.KEY, 'field_shape': 2, 'field_memory': [[0, 0]]}, p) for p in ['matrix', 'decay_rate']],
         ]
     )
-    def test_created_after_function(self, mech_type, mech_kwargs, param_name):
+    def test_created_after_function(self, mech_type, mech_kwargs, param):
         m = mech_type(**mech_kwargs)
-        assert getattr(m.parameters, param_name) in m.parameter_ports
+        param = getattr(m.parameters, param, param)
+        assert param in m.parameter_ports
 
 
 class TestConfigurableParameters:
